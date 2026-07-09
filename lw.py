573e95406b69992b37efdab155d7c4ecf5a4731f
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-07-09 14:50:20 +0530

    added it

--- lw.py
+++ lw.py
@@ -3,5 +3,5 @@ HRA = 0.2*basic;
 DA = 0.1*basic;
 tot_sal = basic+HRA+DA;
 TAX = 0.05*tot_sal;
-net = tot_sal - tax;
-print("Net Salary is",net);
+net = tot_sal - tax; #add some changes
+print("Net Salary  is it",net);
